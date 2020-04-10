ce838ffb0b157ff23756a4f4b8346f83262471f0
Garrett <garrettgsb@gmail.com>
2020-04-10 00:48:49 -0700

    Add 'Audio (all)' button

--- ui/src/components/CitiesList.js
+++ ui/src/components/CitiesList.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import { say } from '../utils.js';
 
+const citySummary = city => {
+  const { city: name, condition, expect, temperature, cloudy, windy } = city;
+  return `Expect ${expect} in ${name}. It's ${temperature} outside, ${cloudy} cloudy, and ${windy} windy.`;
+}
+
 const CitiesList = ({ cities, removeCity }) => (
-  <section className='cities-list'>
-    { cities.map((city, index) => {
-      const { city: name, condition, expect, temperature, cloudy, windy } = city;
-      const summary = `Expect ${expect} in ${name}. It's ${temperature} outside, ${cloudy} cloudy, and ${windy} windy.`;
-      return (
-        <article key={name} className='city-card' styles={{ animationDelay: `${cities.length - index}s` }}>
-          <main>
-            <h2>{city.city}</h2>
-            <h3>{condition}</h3>
-            <p>{summary}</p>
-          </main>
-          <footer>
-            <button onClick={() => say(summary)}>Audio</button>
-            <button onClick={() => removeCity(city.city)}>Delete</button>
-          </footer>
-        </article>
-      );
-    })}
+  <section className='cities-list-container'>
+    <header>
+      {!!cities.length && <button onClick={() => cities.forEach(city => say(citySummary(city)))}>Audio (All)</button>}
+    </header>
+    <main className='cities-list'>
+      { cities.map((city, index) => {
+        const { city: name, condition, expect, temperature, cloudy, windy } = city;
+        const summary = `Expect ${expect} in ${name}. It's ${temperature} outside, ${cloudy} cloudy, and ${windy} windy.`;
+        return (
+          <article key={name} className='city-card' styles={{ animationDelay: `${cities.length - index}s` }}>
+            <main>
+              <h2>{city.city}</h2>
+              <h3>{condition}</h3>
+              <p>{summary}</p>
+            </main>
+            <footer>
+              <button onClick={() => say(summary)}>Audio</button>
+              <button onClick={() => removeCity(city.city)}>Delete</button>
+            </footer>
+          </article>
+        );
+      })}
+    </main>
   </section>
 );
 
